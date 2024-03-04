260e0a24c81736e97c3ae86a4dc9af3f2661f360
Rujek <rujeko24@gmail.com>
2024-03-04 03:57:42 -0800

    Advance

---
+++ 0x00-ES6_basic/100-createIteratorObject.js
@@ -0,0 +1,10 @@
+iexport default function createIteratorObject(report) {
+  let allEmployees = [];
+  for (const item of Object.values(report.allEmployees)) {
+    allEmployees = [
+      ...allEmployees,
+      ...item,
+    ];
+  }
+  return allEmployees;
+}
